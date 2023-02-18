528c5f52fcee3b566c4fb3d1f72a0e604ca231da
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-02-18 17:53:44 +0600

    New design and development

--- js/percentage-calculator-js.js
+++ js/percentage-calculator-js.js
@@ -1,51 +1,87 @@
-_cmnHideElement("OutputResult");
-
-function PercentageCalculatorFormValidate()
+function ValidateCalculatePercentageOfaNumberfeilds()
 {
     RemoveAllErrorMessage();
 
     var percentageOf = document.getElementById("percentageOf").value;
     var percentage = document.getElementById("percentage").value;
-
-    if(IsInputFieldEmpty("percentageOf") || (isNaN(percentageOf) && percentageOf <= 0))
-    {
-        ShowErrorMessageBottomOfTheInputFiled("percentageOf", "Enter correct value.");
-        return false;
-    }
     
     if(IsInputFieldEmpty("percentage") || (isNaN(percentage) && percentage <= 0))
     {
         ShowErrorMessageBottomOfTheInputFiled("percentage", "Enter a value.");
         return false;
-    }   
+    }  
+    
+    if(IsInputFieldEmpty("percentageOf") || (isNaN(percentageOf) && percentageOf <= 0))
+    {
+        ShowErrorMessageBottomOfTheInputFiled("percentageOf", "Enter correct value.");
+        return false;
+    }
 
     return true;
 }
 
-function PercentageCalculatorReset()
+function ValidateCalculatePercentageOfTwoNumberfeilds()
+{
+    RemoveAllErrorMessage();
+
+    var numberOne = document.getElementById("numberOne").value;
+    var numberTwo = document.getElementById("percentageOfNumber").value;
+    
+    if(IsInputFieldEmpty("numberOne") || (isNaN(numberOne) && numberOne <= 0))
+    {
+        ShowErrorMessageBottomOfTheInputFiled("numberOne", "Enter a value.");
+        return false;
+    }  
+    
+    if(IsInputFieldEmpty("percentageOfNumber") || (isNaN(numberTwo) && numberTwo <= 0))
+    {
+        ShowErrorMessageBottomOfTheInputFiled("percentageOfNumber", "Enter correct value.");
+        return false;
+    }
+
+    return true;
+}
+
+function ResetPercentageInputFeilds()
 {
     document.getElementById("percentageOf").value = "";
     document.getElementById("percentage").value = "";
+    document.getElementById("percentageOutputResult").value = "";
 
     RemoveAllErrorMessage();
-
-    _cmnHideElement("OutputResult");
-    _cmnShowElement("OutputInfo", "flex");
 }
 
-function PercentageCalculation()
+function CalculatePercentageOfaNumber()
 {
-    if(PercentageCalculatorFormValidate())
+    if(ValidateCalculatePercentageOfaNumberfeilds())
     {
         var result;
         var percentageOf = Number(document.getElementById("percentageOf").value);
         var percent = Number(document.getElementById("percentage").value);
 
         result = (percentageOf * percent) / 100;
-        document.getElementById("percentResult").innerHTML = Number(result).toFixed(2);
+        document.getElementById("percentageOutputResult").value = Number(result).toFixed(2);
+    }
+}
 
-        //result div show
-        _cmnHideElement("OutputInfo");
-        _cmnShowElement("OutputResult", "flex");
+function ResetTwoNumbersPercentageInputFeilds()
+{
+    document.getElementById("numberOne").value = "";
+    document.getElementById("percentageOfNumber").value = "";
+    document.getElementById("percentResult").value = "";
+
+    RemoveAllErrorMessage();
+}
+
+function CalculatePercentageOfTwoNumbers()
+{
+    if(ValidateCalculatePercentageOfTwoNumberfeilds())
+    {
+        var result;
+        var numberOne = Number(document.getElementById("numberOne").value);
+        var percentageOfNumber = Number(document.getElementById("percentageOfNumber").value);
+
+        result = (numberOne / percentageOfNumber) * 100;
+        document.getElementById("percentResult").value = Number(result).toFixed(2);
     }
 }
